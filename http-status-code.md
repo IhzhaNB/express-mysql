8c9129d831bd5e64a8ade979a67988bf1e17b238
IhzhaNB <ihzhanauvalb@gmail.com>
2023-03-24 14:12:04 +0700

    express mysql commit

---
+++ http-status-code.md
@@ -0,0 +1,23 @@
+# HTTP Status Code
+
+**Successful Response (2xx)**
+
+- 200 OK (GET, PATCH)
+
+- 201 Created (POST, PUT)
+
+**Client Error Response (4xx)**
+
+- 400 Bad Request
+
+- 401 Unaunthorized
+
+- 403 Forbidden
+
+- 404 Not Found
+
+- 405 Method Not Allowed
+
+**Server error response (5xx)**
+
+- 500 Internal Server Error
---
+++ src/index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const express = require("express");
+
+const usersRoutes = require("./routes/users");
+const middlewareLogRequest = require("./middleware/logs");
+const upload = require("./middleware/multer");
+
+const app = express();
+
+// connection mysql
+
+/*  pattern app.method(path, handler) */
+// method
+// app.use("/", (req, res, next) => {
+//   res.send("Hello World");
+// });
+// middleware
+app.use(middlewareLogRequest);
+app.use(express.json());
+app.use("/assets", express.static("public/images"));
+
+// routes
+app.use("/users", usersRoutes);
+app.post("/upload", upload.single("photo"), (req, res) => {
+  res.json({
+    message: "Upload Berhasil",
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.json({
+    message: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server berhasil di running di port ${PORT}`);
+});
